d9e817028f724ae63ed50abd1c2ac9e6e0560729
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-01-15 17:40:06 +0100

    Fixes for QA env

--- run-db-integration-tests.sh
+++ run-db-integration-tests.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+set -euo pipefail
+
+ORCHESTRATOR_CONFIG_URL=$1
+
+cd it
+mvn install -Dcategory=Category1 -Dorchestrator.configUrl=$ORCHESTRATOR_CONFIG_URL -B -e -V
+
--- run-db-unit-tests.sh
+++ run-db-unit-tests.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+
+set -euo pipefail
+
+ORCHESTRATOR_CONFIG_URL=$1
+
+mvn verify -pl :sonar-db -Dorchestrator.configUrl=$ORCHESTRATOR_CONFIG_URL -B -e -V
